f948cf7521becbaf3420f78c9eae3e8a39e6bfc0
meriem beriane <berianebadimeriem@gmail.com>
2021-06-30 14:50:03 +0100

    semi-final

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\facades\Hash;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\WishlistController;
+
+class UserController extends Controller
+{
+    //Register
+    function register(Request $req){
+
+        $user = new User;
+        $user->name = $req->input('name');
+        $user->email = $req->input('email');
+        $user->password = Hash::make($req->input('password'));
+
+        $user->save();
+
+        $user1 = User::where('email', $req->email)->first();
+
+        app('App\Http\Controllers\CartController')->addCart($user->id);
+        app('App\Http\Controllers\WishlistController')->addWishlist($user->id);
+
+        return $user;
+    }
+
+    //login
+    function login(Request $req) {
+        
+        $user = User::where('email', $req->email)->first();
+        if(!$user || !Hash::check($req->password, $user->password)) {
+            return ["error"=> "Email or password is not matched"];
+        }
+
+        return $user;
+    }
+
+    function addWishlist(){
+        $wishlist= new Wishlist;
+
+        $product->name = $req->input('name');
+
+        $product->save();
+
+        return $product;
+    }
+    
+}
---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\WishlistController;
+use App\Http\Controllers\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+
+Route::get('get-cart-products/{id}', [CartController::class, 'getProducts']);
+Route::post('addProductToCart', [ProductController::class, 'addProductToCart']);
+
+Route::get('get-wishlist-products/{id}', [WishlistController::class, 'getProducts']);
+Route::post('addProductToWishlist', [ProductController::class, 'addProductToWishlist']);
+
+Route::delete('delete-product-from-cart', [ProductController::class, 'deleteFromCart']);
+Route::get('delete-product-from-wishlist', [ProductController::class, 'deleteFromWishlist']);
